82f53c9ba192a336deaffd76aac0b28350257abd
Micl <kmleung@live.com>
2022-03-06 20:17:09 +0800

    add stepdefinitionBase

--- StoryTest/StepDefinitions/CallAPIStepDefinitions.cs
+++ StoryTest/StepDefinitions/CallAPIStepDefinitions.cs
@@ -14,56 +14,14 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace P6.StoryTest.StepDefinitions
 {
     [Binding]
-    public class CallAPIStepDefinitions
+    public class CallAPIStepDefinitions : StepDefinitionBase
     {
-        private readonly ScenarioContext context;
-        private readonly WebApplicationFactory<Startup> webApplicationFactory;
-        private string projectDir;
-        private string configPath;
-        private HttpClient client;
-        IConfigurationRoot config;
-
         public CallAPIStepDefinitions(
           ScenarioContext context,
-          WebApplicationFactory<Startup> webApplicationFactory)
+          WebApplicationFactory<Startup> webApplicationFactory) : base(context, webApplicationFactory)
         {
-            // Inject auto test connection string to application under test
-            projectDir = Directory.GetCurrentDirectory();
-            configPath = Path.Combine(projectDir, "appsettings.test.json");
-            config = new ConfigurationBuilder().AddJsonFile(configPath).Build();
-
-            this.context = context;
-            this.webApplicationFactory = webApplicationFactory.WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureAppConfiguration((context, conf) =>
-                {
-                    conf.AddJsonFile(configPath);
-                });
-            });
-
-            // create an http client of the application under test
-            client = this.webApplicationFactory.CreateClient();
-
-            // prepare an empty database for auto test
-            using var provider = new ServiceCollection()
-                //.AddDbContext<Data.EF.EFContext>(options =>
-                //     options.UseSqlServer("Server=(localdb)\\ProjectsV13;Database=DDDSample1;Trusted_Connection=True;MultipleActiveResultSets=true"))
-                .AddDbContext<Data.EF.EFContext>(options =>
-                     options.UseSqlServer(config.GetConnectionString("DDDConnectionString")))
-                .AddScoped<Data.EF.EFContext>()
-                .BuildServiceProvider();
-
-            using (var scope = provider.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<Data.EF.EFContext>();
-                db.Database.ExecuteSqlRaw("Drop Table IF Exists Payslips");
-                db.Database.ExecuteSqlRaw("Drop Table IF Exists Users");
-                db.Database.ExecuteSqlRaw("Drop Table IF Exists Departments");
-                db.Database.ExecuteSqlRaw("Drop Table IF Exists __EFMigrationsHistory");
-                db.Database.Migrate();
-            }
-
         }
+
         [Given(@"I have the following request body:")]
         public void GivenIHaveTheFollowingRequestBody(string multilineText)
         {
---
+++ StoryTest/StepDefinitions/StepDefinitionBase.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http.Headers;
+using System.Net;
+using TechTalk.SpecFlow;
+using Microsoft.AspNetCore.Mvc.Testing;
+using API;
+using Service;
+using Common.DTOs.Users;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace P6.StoryTest.StepDefinitions
+{
+    public class StepDefinitionBase
+    {
+        public readonly ScenarioContext context;
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private string projectDir;
+        private string configPath;
+        public HttpClient client;
+        IConfigurationRoot config;
+        public StepDefinitionBase(
+          ScenarioContext context,
+          WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            // Inject auto test connection string to application under test
+            projectDir = Directory.GetCurrentDirectory();
+            configPath = Path.Combine(projectDir, "appsettings.test.json");
+            config = new ConfigurationBuilder().AddJsonFile(configPath).Build();
+
+            this.context = context;
+            this.webApplicationFactory = webApplicationFactory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, conf) =>
+                {
+                    conf.AddJsonFile(configPath);
+                });
+            });
+
+            // create an http client of the application under test
+            client = this.webApplicationFactory.CreateClient();
+
+            // prepare an empty database for auto test
+            using var provider = new ServiceCollection()
+                .AddDbContext<Data.EF.EFContext>(options =>
+                     options.UseSqlServer(config.GetConnectionString("DDDConnectionString")))
+                .AddScoped<Data.EF.EFContext>()
+                .BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<Data.EF.EFContext>();
+                db.Database.ExecuteSqlRaw("Drop Table IF Exists Payslips");
+                db.Database.ExecuteSqlRaw("Drop Table IF Exists Users");
+                db.Database.ExecuteSqlRaw("Drop Table IF Exists Departments");
+                db.Database.ExecuteSqlRaw("Drop Table IF Exists __EFMigrationsHistory");
+                db.Database.Migrate();
+            }
+        }
+    }
+}
